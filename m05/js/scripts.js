faa1b7d817e99c31cb3fd88418351fd079dbb5d3
Avollmar <alan.vollmar@bellevuecollege.edu>
2025-02-03 13:44:46 -0800

    validator changes

--- m05/js/scripts.js
+++ m05/js/scripts.js
@@ -2,25 +2,25 @@
 
 var userSelection = prompt("Please enter a number between 0 and 10. \nNOTE: The result with be rounded to the nearest whole number.", "7");
 
- function inputCheck(input) {  //Checks if user input is an integer
+ function inputCheck(input) {  //Validates input
 
-  if (isNaN(input) == false && input >10) {
+  if (isNaN(input) == false && input >10) { //If greater than 10, alert and reload/reprompt
     alert("BETWEEN 0 and 10, silly. Try again.");
-    window.location.reload();
+    window.location.reload(); //Reload page
     return false;
   }
 
-  if (isNaN(input) || input > 10 || input == "0" || input < 0) { //Zero edge case.
+  if (isNaN(input) || input > 10 || input == "0" || input < 0) { //NaN or 0 conditional
     return 0;
-  } else {
+  } else { //Validated input makes it here
     var result = Math.round(input);  //Round to nearest whole number
     return result; //Returns sanitized input
   }
 }
 
-var table = inputCheck(userSelection);
+var table = inputCheck(userSelection); //Run function above and use return value as final user input
 var i = 1; // Initialize counter to 1
-var msg = '<h2>Multiplication Table</h2>'; // Message
+var msg = '<h2>Multiplication Table</h2>'; //Populate heading
 
 //Stripped out operator conditional.
 
