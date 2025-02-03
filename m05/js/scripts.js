bac1d22b121c5bc517bd3ac5819d89cb94c49b66
Avollmar <alan.vollmar@bellevuecollege.edu>
2025-02-03 13:19:30 -0800

    lumping NaN and 0 into one bucket: 5-13

--- m05/js/scripts.js
+++ m05/js/scripts.js
@@ -2,28 +2,22 @@
 
 var userSelection = prompt("Please enter a number between 0 and 10. \nNOTE: The result with be rounded to the nearest whole number.", "7");
 
- function integerCheck(input) {  //Checks if user input is an integer
-  if (input > 10 || input == "0" || input < 0) { //Range, including 0 edge case.
-    alert("BETWEEN 0 and 10, pal. Try again");
-    window.location.reload(); //Lazy workaround, need to add new prompt without alert spam
-    return false;
-  }
-
-  if (isNaN(input) == false) {  //Input is an integer and within range
+ function inputCheck(input) {  //Checks if user input is an integer
+  if (isNaN(input) || input > 10 || input == "0" || input < 0) { //Zero edge case.
+    input = 0;
+    return input;
+  } else {
     input = Math.round(input);  //Round to nearest whole number
     return input; //Returns sanitized input
-  } else {  //Contains characters other than numbers
-    alert("Read the instructions, silly. Try again");
-    window.location.reload(); //Lazy workaround, need to add new prompt without alert spam
   }
 }
 
-if (integerCheck(userSelection)) {
-  var table = integerCheck(userSelection);
+if (inputCheck(userSelection)) {
+  var table = inputCheck(userSelection);
   var i = 1; // Initialize counter to 1
   var msg = '<h2>Multiplication Table</h2>'; // Message
 
-  //Stripped out conditional statement altogether.
+  //Stripped out operator conditional.
 
   while (i < 11) {
     msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
