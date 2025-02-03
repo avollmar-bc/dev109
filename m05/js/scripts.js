ba666a9530d7b0536ff6572fb7c7789ee2736dae
Avollmar <alan.vollmar@bellevuecollege.edu>
2025-02-03 10:26:24 -0800

    Cleaned up JS, only multiplication.

--- m05/js/scripts.js
+++ m05/js/scripts.js
@@ -1,22 +1,14 @@
 /*-- Imported from: https://javascriptbook.com/code/c04/js/example.js --*/
 
 var table = 3;             // Unit of table
-var operator = 'addition'; // Type of calculation
+var operator = 'multiplication'; // Type of calculation
 var i = 1;                 // Set counter to 1
-var msg = '';              // Message
+var msg = 'Multiplication Table';              // Message
 
-if (operator === 'addition') {
-  // Do addition
-  while (i < 11) {
-    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
-    i++;
-  }
-} else {
-  // Do multiplication
-  while (i < 11) {
-    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
-    i++;
-  }
+//Stripped out conditional statement altogether.
+while (i < 11) {
+  msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+  i++;
 }
 
 // Write the message into the page
