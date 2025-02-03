1b562228012eb199e0b229219974e6c9acc25ce0
Avollmar <alan.vollmar@bellevuecollege.edu>
2025-02-03 13:24:50 -0800

    logic changes.

--- m05/js/scripts.js
+++ m05/js/scripts.js
@@ -4,27 +4,24 @@ var userSelection = prompt("Please enter a number between 0 and 10. \nNOTE: The 
 
  function inputCheck(input) {  //Checks if user input is an integer
   if (isNaN(input) || input > 10 || input == "0" || input < 0) { //Zero edge case.
-    input = 0;
-    return input;
+    return 0;
   } else {
-    input = Math.round(input);  //Round to nearest whole number
-    return input; //Returns sanitized input
+    var result = Math.round(input);  //Round to nearest whole number
+    return result; //Returns sanitized input
   }
 }
 
-if (inputCheck(userSelection)) {
-  var table = inputCheck(userSelection);
-  var i = 1; // Initialize counter to 1
-  var msg = '<h2>Multiplication Table</h2>'; // Message
+var table = inputCheck(userSelection);
+var i = 1; // Initialize counter to 1
+var msg = '<h2>Multiplication Table</h2>'; // Message
 
-  //Stripped out operator conditional.
+//Stripped out operator conditional.
 
-  while (i < 11) {
-    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
-    i++;
-}
+while (i < 11) {
+  msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+  i++;
 
-  // Write the message into the page
-  var el = document.getElementById('blackboard');
-  el.innerHTML = msg;
+// Write the message into the page
+var el = document.getElementById('blackboard');
+el.innerHTML = msg;
 }
