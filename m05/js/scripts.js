0d759a64395841eb7cad515b83f033a544e3d3de
Avollmar <alan.vollmar@bellevuecollege.edu>
2025-02-03 12:29:51 -0800

    Meh, last bit of DOM loading tests

--- m05/js/scripts.js
+++ m05/js/scripts.js
@@ -1,17 +1,6 @@
 /*-- Imported from: https://javascriptbook.com/code/c04/js/example.js --*/
 
-
-function docReady() { // see if DOM is already available
-  if (document.readyState === "complete" || document.readyState === "interactive") {
-      setTimeout(userSelection, 100); //100ms timeout
-  } else {
-      document.addEventListener("DOMContentLoaded", function() {
-        var userSelection = prompt("Please enter a number between 0 and 10. \nNOTE: The result with be rounded to the nearest whole number.", "7");
-      }); //Rerun prompt
-  }
-}
-
-docReady();
+var userSelection = prompt("Please enter a number between 0 and 10. \nNOTE: The result with be rounded to the nearest whole number.", "7");
 
  function integerCheck(input) {  //Checks if user input is an integer
   if (isNaN(input) == false && input <= 10 && input > 0) {  //Input is an integer and within range
