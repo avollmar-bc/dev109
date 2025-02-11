c05e136b49ad6768d497e72d064e82894b05c727
Avollmar <alan.vollmar@bellevuecollege.edu>
2025-02-11 10:13:28 -0800

    Testing 2 section layout

--- a3/js/scripts.js
+++ a3/js/scripts.js
@@ -109,3 +109,26 @@ function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
         
         document.getElementById("downLeft").innerHTML = rLine;
         }
+
+        //Top Area
+        function topShape(pHeight, pColorEven, pColorOdd, pSymbol) {
+            var tLine = "";
+            for (i = 0; i < pHeight*2; i++) {
+                rLine += "<p>";
+
+                for (j = 0; j <=i; j++) {
+                    if (j%2)
+                        //even
+                        rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+                        else
+                        //odd
+                        rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+                        
+                }
+                
+                rLine +="</p>";
+            }
+
+            document.getElementById("topShape").innerHTML = rLine;
+
+        }
