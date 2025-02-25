8448c218dd67d186bc4929adc0f19905b6e41b8f
Avollmar <alan.vollmar@bellevuecollege.edu>
2025-02-25 10:23:14 -0800

    initial js

---
+++ a4/js/scripts.js
@@ -0,0 +1,20 @@
+function validateForm() {
+
+    //Collect field values
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var userEmail = document.getElementById("userEmail").value;
+    var phone = document.getElementById("phone").value;
+    var userName = document.getElementById("userName").value;
+    var password = document.getElementById("password").value;
+    var address = document.getElementById("address").value;
+    var cityName = document.getElementById("cityName").value;
+    var statename = document.getElementById("stateName").value;
+    var countryName = document.getElementById("countryName").value;
+    var zipCode = document.getElementById("zipCode").value;
+    var comments = document.getElementById("comments").value;
+
+    //Blanket validation flag
+    var isValid = true;
+
+}
