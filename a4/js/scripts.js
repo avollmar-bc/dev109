6f28e0d58d96472a8e7f9926197dc572750875fd
Avollmar <alan.vollmar@bellevuecollege.edu>
2025-02-26 14:37:33 -0800

    form submission

--- a4/js/scripts.js
+++ a4/js/scripts.js
@@ -1,43 +1,5 @@
 function validateForm() {
 
-    //Collect field values
-    var firstName = document.getElementById("firstName").value;
-    var lastName = document.getElementById("lastName").value;
-    var userEmail = document.getElementById("userEmail").value;
-    var phone = document.getElementById("phone").value;
-    var userName = document.getElementById("userName").value;
-    var password = document.getElementById("password").value;
-    var address = document.getElementById("address").value;
-    var cityName = document.getElementById("cityName").value;
-    var statename = document.getElementById("stateName").value;
-    var countryName = document.getElementById("countryName").value;
-    var zipCode = document.getElementById("zipCode").value;
-    var comments = document.getElementById("comments").value;
-
-    //Error Fields
-    var firstNameError = document.getElementById("firstName-error");
-    var lastNameError = document.getElementById("lastName-error");
-    var userEmailError = document.getElementById("userEmail-error");
-    var phoneError = document.getElementById("phone-error");
-    var userNameError = document.getElementById("userName-error");
-    var passwordError = document.getElementById("password-error");
-    var addressError = document.getElementById("address-error");
-    var cityNameError = document.getElementById("cityName-error");
-    var statenameError = document.getElementById("stateName-error");
-    var countryNameError = document.getElementById("countryName-error");
-    var zipCodeError = document.getElementById("zipCode-error");
-    var commentsError = document.getElementById("comments-error");
-
-    //Blanket validation flag
-    var isValid = true;
-
-    //Regexs
-
-    //Alpha only - up to 20 characters
-    var alphaTest = /^[a-zA-Z]{1,20}$/;
-    var emailTest = /^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/;
-    // old test - var emailTest = /^\S+@\S+\.\S+$/;
-
     //First Name Field
     if (!alphaTest.test(firstName)) {
         firstNameError.innerHTML = 'The First Name field must contain up to 20 alphabetical characters.';
