b601fd3dbc3757f9e3a4cd602face59befa092fc
Avollmar <alan.vollmar@bellevuecollege.edu>
2025-02-26 08:06:52 -0800

    phone formatting

--- a4/js/scripts.js
+++ a4/js/scripts.js
@@ -127,3 +127,27 @@ phone.addEventListener("blur", (event) => {
         phoneError.classList.remove("active-error");
     }
 });
+
+window.addEventListener('load', () => {
+    const phoneInput = document.getElementById("phone");
+    phoneInput.addEventListener('keydown', disallowNonNumericInput);
+    phoneInput.addEventListener('keyup', formatToPhone);
+  });
+  
+  const disallowNonNumericInput = (evt) => {
+    if (evt.ctrlKey) { return; }
+    if (evt.key.length > 1) { return; }
+    if (/[0-9.]/.test(evt.key)) { return; }
+    evt.preventDefault();
+  }
+  
+  const formatToPhone = (evt) => {
+    const digits = evt.target.value.replace(/\D/g,'').substring(0,10);
+    const areaCode = digits.substring(0,3);
+    const prefix = digits.substring(3,6);
+    const suffix = digits.substring(6,10);
+  
+    if(digits.length > 6) {evt.target.value = `(${areaCode}) ${prefix} - ${suffix}`;}
+    else if(digits.length > 3) {evt.target.value = `(${areaCode}) ${prefix}`;}
+    else if(digits.length > 0) {evt.target.value = `(${areaCode}`;}
+  };
