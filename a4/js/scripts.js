ff4d87fb530bdb08ef499e9f67daf0c486928d2a
Avollmar <alan.vollmar@bellevuecollege.edu>
2025-02-26 09:04:46 -0800

    password testing

--- a4/js/scripts.js
+++ a4/js/scripts.js
@@ -178,3 +178,18 @@ userName.addEventListener("blur", (event) => {
         userNameError.classList.remove("active-error");
     }
 });
+
+//Password Validation
+var password = document.getElementById("password");
+var passwordError = document.getElementById("password-error");
+
+userName.addEventListener("blur", (event) => {
+    if (!passwordTest.test(password.value)) {
+        passwordError.innerHTML = 'Your password must contain at least ONE uppercase, ONE lowercase, ONE number, and ONE special character.';
+        passwordError.classList.add("active-error");
+        isValid = false;
+    } else {
+        passwordError.innerHTML = '';
+        passwordError.classList.remove("active-error");
+    }
+});
