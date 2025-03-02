1cdd7256790ce5c0921ca4b85f4421a24eba549a
Avollmar <alan.vollmar@bellevuecollege.edu>
2025-03-01 23:12:06 -0800

    testing slideshow

---
+++ a5/js/scripts.js
@@ -0,0 +1,36 @@
+var myImages =["../images/slideshow-1.jpg", "../images/slideshow-2.jpg","../images/slideshow-3.jpg","../images/slideshow-4.jpg","../images/slideshow-5.jpg"];
+
+
+var captionImages =["Cat 1","Cat 2","Cat 3","Cat 4","Cat 5"];
+
+ var index=0; 
+
+ function updateImage(){
+ document.getElementById("slideshow").src = myImages[index];
+ document.getElementById("slideshow").alt= captionImages[index];
+ document.getElementById("caption").textContent = captionImages[index]; 
+} 
+
+function next(){
+ if (myImages.length == index+1)
+ index=0;
+ else
+ index++;
+ updateImage();
+} 
+ 
+
+function back(){
+ if (index===0)
+ index=myImages.length-1;
+ else
+ index--;
+ 
+ updateImage();
+} 
+
+var nextButton = document.getElementById("next"); 
+var previousButton = document.getElementById("previous"); 
+
+previousButton.addEventListener("click",back,false);
+nextButton.addEventListener("click",next,false); 
