08e9bdf0b7a412681644898f62d5559e9508b344
Avollmar <alan.vollmar@bellevuecollege.edu>
2025-03-01 22:09:31 -0800

    testing

--- m09/js/scripts.js
+++ m09/js/scripts.js
@@ -7,19 +7,12 @@ for (index = element.length - 1; index >= 0; index--) {
     element[index].parentNode.removeChild(element[index]);
 }
 
-var colorChoice = document.getElementById("colorChoice");
-console.log(colorChoice);
-
-colorChoice.addEventListener("change", function(e) {
-    console.log(colorChoice.value);
-    console.log("Butts: " + e.value);
-});
-
 event.stopPropagation();
   });
   addEventListener("click", function(event) {
     var dot = document.createElement("div");
     dot.className = "dot";
+    dot.className = "dots" + "test";
     dot.style.left = (event.pageX - 4) + "px";
     dot.style.top = (event.pageY - 4) + "px";
     document.body.appendChild(dot);
