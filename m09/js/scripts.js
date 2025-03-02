f51d48dc02c0e7e83b1213d2e40002ce4604b4c8
Avollmar <alan.vollmar@bellevuecollege.edu>
2025-03-01 22:23:04 -0800

    dot size + radius

--- m09/js/scripts.js
+++ m09/js/scripts.js
@@ -12,9 +12,11 @@ event.stopPropagation();
   addEventListener("click", function(event) {
     var colorChoice = document.getElementById("colorChoice").value;
     var sizeChoice = document.getElementById("sizeSlider").value;
+    var halfSize = sizeChoice / 2;
     var dot = document.createElement("div");
     dot.style.height = sizeChoice + "px";
     dot.style.width = sizeChoice + "px";
+    dot.style.borderRadius = halfSize + "px";
     dot.className = "dot " + colorChoice;
     dot.style.left = (event.pageX - 4) + "px";
     dot.style.top = (event.pageY - 4) + "px";
