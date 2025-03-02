232daf7107b8a095fa5155af3b3a9c75c1fdff62
Avollmar <alan.vollmar@bellevuecollege.edu>
2025-03-01 22:20:12 -0800

    testing size

--- m09/js/scripts.js
+++ m09/js/scripts.js
@@ -11,7 +11,10 @@ event.stopPropagation();
   });
   addEventListener("click", function(event) {
     var colorChoice = document.getElementById("colorChoice").value;
+    var sizeChoice = document.getElementById("sizeSlider").value;
     var dot = document.createElement("div");
+    dot.style.height = sizeChoice + "px";
+    dot.style.width = sizeChoice + "px";
     dot.className = "dot " + colorChoice;
     dot.style.left = (event.pageX - 4) + "px";
     dot.style.top = (event.pageY - 4) + "px";
